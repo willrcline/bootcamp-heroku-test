e7d603513a088e756ea86a2e4df1ea88af18680b
willcline613 <willrcline.atx@gmail.com>
2023-05-03 13:38:13 -0500

    reduced complexity

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+// // GET Route for feedback page
+// app.get('/feedback', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
